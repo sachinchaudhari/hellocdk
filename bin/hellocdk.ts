30279a2beb6460d26a4f10f339a9eab2b26fce02
SChaudhari <schauhari@beachbody.com>
2020-12-23 01:50:49 -0500

    lambda

--- bin/hellocdk.ts
+++ bin/hellocdk.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { HellocdkStack } from '../lib/hellocdk-stack';
+import * as cdk from "@aws-cdk/core";
+import { HellocdkStack } from "../lib/hellocdk-stack";
 
 const app = new cdk.App();
-new HellocdkStack(app, 'HellocdkStack');
+new HellocdkStack(app, "HellocdkStack");
